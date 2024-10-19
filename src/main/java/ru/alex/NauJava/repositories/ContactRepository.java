344984074326b41fc5a7287c56065c83372f85e4
Alex Nikolaev <nlxn@yandex.ru>
2024-10-19 20:44:02 +0500

    Remove unused code

--- src/main/java/ru/alex/NauJava/repositories/ContactRepository.java
+++ src/main/java/ru/alex/NauJava/repositories/ContactRepository.java
@@ -1,10 +1,8 @@
 package ru.alex.NauJava.repositories;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 import ru.alex.NauJava.entities.Contact;
 
 import java.util.List;
@@ -14,8 +12,4 @@ public interface ContactRepository extends CrudRepository<Contact, Long> {
 
     @Query("SELECT c FROM Contact c JOIN c.groups g WHERE g.name = :groupName")
     List<Contact> findContactsByGroupName(@Param("groupName") String groupName);
-
-    @Modifying
-    @Query("DELETE FROM Contact c WHERE c.id IN :ids")
-    int deleteContactsByIds(@Param("ids") List<Long> ids);
 }
